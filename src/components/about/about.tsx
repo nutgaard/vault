5a0a347c9b0c4258205bd0bc86b750cb7eb03adc
Nicklas Utgaard <nutgaard@gmail.com>
2021-01-24 22:21:38 +0100

    Set about-modal to be closed by default

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -10,7 +10,7 @@ interface Props extends HtmlProps {
 }
 
 function About(props: Props) {
-    const [isOpen, setIsOpen] = useState<boolean>(true);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
     const open = useCallback(() => setIsOpen(true), [setIsOpen]);
     const close = useCallback(() => setIsOpen(false), [setIsOpen]);
 
